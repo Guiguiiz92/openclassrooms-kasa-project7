0bb3a45f6faab5cf9d5fa73020296ce758ef86eb
Guittonneau <guittonneau92@sfr.fr>
2023-07-18 17:04:21 +0200

    Correction mentorat

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,29 @@
-
-
 const Banner = (props) => {
-    return (
-        <div id="banner" className={props.className} style={{
-            backgroundImage : `linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3) ), url(${props.img})`,
-            }}>
-                
-            <div id="bannerTexte">{props.txt && props.txt.map((value,index)=>{
-                return <span key={index}>{value}</span>
-            })}</div>
-        </div>
-       
-    )
-}
+	return (
+		// <div
+		// 	id="banner"
+		// 	className={props.className}
+		// 	style={{
+		// 		backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3),rgba(0, 0, 0, 0.3) ), url(${props.img})`,
+		// 	}}
+		// >
+		<div className="banner">
+			<img
+				id="bannerImg"
+				className={props.className}
+				src={props.img}
+				alt="toto"
+			/>
+			<div className="filigrane" />
+			<div id="bannerTexte">
+				{props.txt &&
+					props.txt.map((value, index) => {
+						return <span key={index}>{value}</span>;
+					})}
+			</div>
+		</div>
+		// </div>
+	);
+};
 
-export default Banner
+export default Banner;
